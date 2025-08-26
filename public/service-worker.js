e972a60a5c49653a2ca845b68859adc5abb83321
TitusP29 <tituskubayi5@gmail.com>
2025-08-27 00:49:10 +0200

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,41 @@
+// service-worker.js
+// Basic service worker for offline caching of static assets
+
+const CACHE_NAME = 'kasi-rooms-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/favicon.ico',
+  '/manifest.json',
+  // add more files/assets as needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
